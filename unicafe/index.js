0b48548404b276267462cdc63a79cc4f489fd490
jusalanen <jussialanen9@gmail.com>
2019-07-12 21:50:16 +0300

    lisää tehtävä 1.6 unicafe

---
+++ unicafe/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
+      <h2>statistics</h2>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));

8152773bd5dd04bac89c3a2e63f99118fc2a993b
jusalanen <jussialanen9@gmail.com>
2019-07-14 15:47:29 +0300

    lisää tehtävä 1.8

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -8,27 +8,36 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad
+  const average = (props.good - props.bad)/all
+  const positive = props.good * 100/all
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good: {props.good}</p>
+      <p>neutral: {props.neutral}</p>
+      <p>bad: {props.bad}</p>
+      <p>all: {all}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
-  const average = (good - bad)/(good + neutral + bad)
-  const positive = good * 100/(good + neutral + bad)
-
+  
   return (
     <div>
       <h1>Give feedback</h1>
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
-      <h2>statistics</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {all}</p>
-      <p>average: {average}</p>
-      <p>positive: {positive} %</p>
+      <Statistics good = {good} neutral = {neutral} bad = {bad} />      
     </div>
   )
 }
